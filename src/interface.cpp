ae9e9778b5b63b8796a9dcf65cc17f069793ac15
lordgkram <52086058+lordgkram@users.noreply.github.com>
2023-08-23 00:42:11 +0200

    made formatting more uniform & made mqttCallback use std::string_view

--- src/interface.cpp
+++ src/interface.cpp
@@ -5,138 +5,127 @@
 #include <ArduinoJson.h>
 
 void sendIrcMessage(String &s) {
-  if (s.length() > 500) {
-    while(s.length() > 500){
-      psClient.publish(MQTT_IRC_TX, s.substring(0, 500).c_str());
-      s.remove(0, 500);
-      if (s.length() > 0) {
-        psClient.publish(MQTT_IRC_TX, s.c_str());
-      }
-    }
-  } else
-    psClient.publish(MQTT_IRC_TX, s.c_str());
+    sendIrcMessage(s.c_str());
 }
 
 void sendIrcMessage(const char *s) {
-  size_t len = strlen(s);
-  if (len > 500) {
-    size_t i = 0;
-    while((len - i) >= 500) {
-      psClient.publish(MQTT_IRC_TX, s + i, 500);
-      i += 500;
-    }
-    if(len - i) psClient.publish(MQTT_IRC_TX, s + i, len - i);
-  } else
-    psClient.publish(MQTT_IRC_TX, s);
+    size_t len = strlen(s);
+    if(len > 500) {
+        size_t i = 0;
+        while((len - i) >= 500) {
+            psClient.publish(MQTT_IRC_TX, s + i, 500);
+            i += 500;
+        }
+        if(len - i) psClient.publish(MQTT_IRC_TX, s + i, len - i);
+    } else
+        psClient.publish(MQTT_IRC_TX, s);
 }
 
 void setMusicStatus(bool newStatus){
-  if(newStatus)
-    psClient.publish(MQTT_MUSIC_ON_TOPIC, MQTT_MUSIC_ON_MESSAGE);
-  else
-    psClient.publish(MQTT_MUSIC_OFF_TOPIC, MQTT_MUSIC_OFF_MESSAGE);
+    if(newStatus)
+        psClient.publish(MQTT_MUSIC_ON_TOPIC, MQTT_MUSIC_ON_MESSAGE);
+    else
+        psClient.publish(MQTT_MUSIC_OFF_TOPIC, MQTT_MUSIC_OFF_MESSAGE);
 }
 
 void mqttCallback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (unsigned int i=0;i<length;i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
-  String strTopic = topic;
-  if(strTopic.equals(MQTT_PLAYMIDI)) {
-    payload[length] = '\0';
-    Serial.println("play midi vom mqtt erkannt");
-    // JSON MIDI
-    DynamicJsonDocument data(768);
-    auto jsonErr = deserializeJson(data, payload);
-    if(jsonErr != DeserializationError::Ok) {
-      // backwards compatibility
-      projektionMidiPlayer->enqueue((char*)payload, 16);
-      return;
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+    for(unsigned int i = 0; i < length; i++) {
+        Serial.print((char) payload[i]);
     }
-    String midi = data["midi"];
-    uint16_t length = data.containsKey("laenge") ? data["laenge"] : data["length"];
+    Serial.println();
 
-    bool allowBuffer = data.containsKey("aktiviereBuffer") ? data["aktiviereBuffer"] : data["allowBuffer"];
-    if(allowBuffer) {
-      String user = data.containsKey("nutzer") ? data["nutzer"] : data["user"];
-      uint16_t maxBufferSize = data.containsKey("maximaleBufferGroesse") ? data["maximaleBufferGroesse"] : data["maxBufferSize"];
+    String strTopic = topic;
+    if(strTopic.equals(MQTT_PLAYMIDI)) {
+        std::string_view payloadStr((char *) payload, length);
+        Serial.println("play midi vom mqtt erkannt");
+        // JSON MIDI
+        DynamicJsonDocument data(768);
+        auto jsonErr = deserializeJson(data, payloadStr);
+        if(jsonErr != DeserializationError::Ok) {
+            // backwards compatibility
+            projektionMidiPlayer->enqueue(std::string(payloadStr), 16);
+            return;
+        }
+        String midi = data["midi"];
+        uint16_t length = data.containsKey("laenge") ? data["laenge"] : data["length"];
 
-      uint32_t currBufferLength = 0;
-      uint8_t out = projektionMidiPlayer->bufferOperation(midi.c_str(), user.c_str(), length, maxBufferSize, currBufferLength);
-      if(out) {
-        String text = "(MIDI) @" + user;
-        bool sendText = false;
+        bool allowBuffer = data.containsKey("aktiviereBuffer") ? data["aktiviereBuffer"] : data["allowBuffer"];
+        if(allowBuffer) {
+            String user = data.containsKey("nutzer") ? data["nutzer"] : data["user"];
+            uint16_t maxBufferSize = data.containsKey("maximaleBufferGroesse") ? data["maximaleBufferGroesse"] : data["maxBufferSize"];
 
-        // delete
-        switch((out >> 0) & 3) {
-        case 1:
-          sendText = true;
-          text += " Dein Puffer wurde erfolgreich gelöscht!";
-          break;
-        case 2:
-          sendText = true;
-          text += " Du hast keinen Puffer!";
-          break;
-        default:
-          break;
-        }
-        // create
-        switch((out >> 2) & 3) {
-        case 1:
-          sendText = true;
-          text += " Puffer wurde erfolgreich erschaffen!";
-          break;
-        case 2:
-          sendText = true;
-          text += " Du hast bereits einen Puffer.";
-          break;
-        case 3:
-          sendText = true;
-          text += " Es konnte kein Puffer erschaffen werden.";
-          break;
-        default:
-          break;
-        }
-        // modified
-        if((out >> 4) & 1) {
-          sendText = true;
-          text += " Daten wurden zu deinem Puffer hinzugefügt (";
-          text += currBufferLength;
-          text += "/";
-          text += maxBufferSize;
-          text += ")";
-        }
-        // refund
-        if((out >> 5) & 1) {
-          if(data.containsKey("refundID") && data.containsKey("refundTopic")) {
-            psClient.publish(data["refundTopic"], data["refundID"]);
-          }
-        }
-        // faild enqueue
-        if((out >> 6) & 1) {
-          sendText = true;
-          text += " Die Warteschlange ist voll!";
-        }
+            uint32_t currBufferLength = 0;
+            uint8_t out = projektionMidiPlayer->bufferOperation(midi.c_str(), user.c_str(), length, maxBufferSize, currBufferLength);
+            if(out) {
+                String text = "(MIDI) @" + user;
+                bool sendText = false;
 
-        if(sendText) sendIrcMessage(text);
-      }
-    } else {
-      if(!projektionMidiPlayer->enqueue(midi.c_str(), length)) {
-        sendIrcMessage("(MIDI) Die Warteschlange ist voll!");
-        if(data.containsKey("refundID") && data.containsKey("refundTopic")) {
-          psClient.publish(data["refundTopic"], data["refundID"]);
+                // delete
+                switch((out >> 0) & 3) {
+                case 1:
+                    sendText = true;
+                    text += " Dein Puffer wurde erfolgreich gelöscht!";
+                    break;
+                case 2:
+                    sendText = true;
+                    text += " Du hast keinen Puffer!";
+                    break;
+                default:
+                    break;
+                }
+                // create
+                switch((out >> 2) & 3) {
+                case 1:
+                    sendText = true;
+                    text += " Puffer wurde erfolgreich erschaffen!";
+                    break;
+                case 2:
+                    sendText = true;
+                    text += " Du hast bereits einen Puffer.";
+                    break;
+                case 3:
+                    sendText = true;
+                    text += " Es konnte kein Puffer erschaffen werden.";
+                    break;
+                default:
+                    break;
+                }
+                // modified
+                if((out >> 4) & 1) {
+                    sendText = true;
+                    text += " Daten wurden zu deinem Puffer hinzugefügt (";
+                    text += currBufferLength;
+                    text += "/";
+                    text += maxBufferSize;
+                    text += ")";
+                }
+                // refund
+                if((out >> 5) & 1) {
+                    if(data.containsKey("refundID") && data.containsKey("refundTopic")) {
+                        psClient.publish(data["refundTopic"], data["refundID"]);
+                    }
+                }
+                // faild enqueue
+                if((out >> 6) & 1) {
+                    sendText = true;
+                    text += " Die Warteschlange ist voll!";
+                }
+
+                if(sendText) sendIrcMessage(text);
+            }
+        } else {
+            if(!projektionMidiPlayer->enqueue(midi.c_str(), length)) {
+                sendIrcMessage("(MIDI) Die Warteschlange ist voll!");
+                if(data.containsKey("refundID") && data.containsKey("refundTopic")) {
+                    psClient.publish(data["refundTopic"], data["refundID"]);
+                }
+            }
         }
-      }
+    } else if(strTopic.equals(MQTT_KILLMIDI)) {
+        Serial.println("kill midi vom mqtt erkannt");
+        projektionMidiPlayer->kill();
     }
-  } else if(strTopic.equals(MQTT_KILLMIDI)) {
-    payload[length] = '\0';
-    Serial.println("kill midi vom mqtt erkannt");
-    projektionMidiPlayer->kill();
-  }
-
 }
--- src/main.cpp
+++ src/main.cpp
@@ -13,21 +13,21 @@ adruinoTestOut *testOut;
 silentAdruinoTestOut *silentTestOut;
 
 void setup() {
-  Serial.begin(115200);
-  testOut = new adruinoTestOut();
-  silentTestOut = new silentAdruinoTestOut();
-  ok = !test(testOut);
-  trys++;
-  delay(10000);
+    Serial.begin(115200);
+    testOut = new adruinoTestOut();
+    silentTestOut = new silentAdruinoTestOut();
+    ok = !test(testOut);
+    trys++;
+    delay(10000);
 }
 
 void loop() {
-  if(!ok && trys < TEST_COUNT) {
-    Serial.println("--------------------------------------------------------------------------------");
-    ok = !test(silentTestOut);
-    trys++;
-  } else if(!ok && trys == TEST_COUNT) { Serial.println("Canceling Tests!"); trys++; }
-  delay(10000);
+    if(!ok && trys < TEST_COUNT) {
+        Serial.println("--------------------------------------------------------------------------------");
+        ok = !test(silentTestOut);
+        trys++;
+    } else if(!ok && trys == TEST_COUNT) { Serial.println("Canceling Tests!"); trys++; }
+    delay(10000);
 }
 
 #else /* RUN_TEST */
@@ -66,101 +66,99 @@ PubSubClient psClient(wiFiClient);
 
 
 void mqttReconnect() {
-  // Loop until we're reconnected
-  while (!psClient.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
+    // Loop until we're reconnected
+    while(!psClient.connected()) {
+        Serial.print("Attempting MQTT connection...");
+        // Attempt to connect
 #if noMqttUser
-    if (psClient.connect(MQTT_CLIENT_ID))
+        if(psClient.connect(MQTT_CLIENT_ID))
 #else
-    if (psClient.connect(MQTT_CLIENT_ID, mqttUser, mqttPassword))
+        if(psClient.connect(MQTT_CLIENT_ID, mqttUser, mqttPassword))
 #endif
-    {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      //psClient.publish("wled/861a06/api","%PL=10");
-      // ... and resubscribe
-      psClient.subscribe(MQTT_PLAYMIDI, 1);
-      psClient.subscribe(MQTT_KILLMIDI, 1);
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(psClient.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+        {
+            Serial.println("connected");
+            // Once connected, publish an announcement...
+            //psClient.publish("wled/861a06/api","%PL=10");
+            // ... and resubscribe
+            psClient.subscribe(MQTT_PLAYMIDI, 1);
+            psClient.subscribe(MQTT_KILLMIDI, 1);
+        } else {
+            Serial.print("failed, rc=");
+            Serial.print(psClient.state());
+            Serial.println(" try again in 5 seconds");
+            // Wait 5 seconds before retrying
+            delay(5000);
+        }
     }
-  }
 }
 
-void setup()
-{
-  //Set up serial output with standard MIDI baud rate
+void setup() {
+    //Set up serial output with standard MIDI baud rate
 
-  Serial.begin(115200);
-  Serial.println();
+    Serial.begin(115200);
+    Serial.println();
 
-  wifictrl.setupWifiPortal("MidiPlayer");
+    wifictrl.setupWifiPortal("MidiPlayer");
 
-  ArduinoOTA.setHostname(OTA_HOST);
-  if (OTA_PORT != 0)
-    ArduinoOTA.setPort(OTA_PORT);
-  ArduinoOTA.begin();
+    ArduinoOTA.setHostname(OTA_HOST);
+    if(OTA_PORT != 0)
+        ArduinoOTA.setPort(OTA_PORT);
+    ArduinoOTA.begin();
 
-  projektionMidiPlayer = new projektionMidi::projektionMidi();
-  projektionMidiPlayer->setMusicStatusReciever(setMusicStatus);
-  projektionMidiPlayer->setErrorReciever([](const char *msg) {
-    size_t len = strlen(msg);
-    const char *prefix = "(MIDI:ERROR) ";
-    size_t prefixLen = strlen(prefix);
-    char *out = (char *) malloc(len + prefixLen + 1);
-    memcpy(out, prefix, prefixLen);
-    memcpy(out + prefixLen, msg, len + 1);
-    sendIrcMessage(out);
-    Serial.printf("MIDI:ERR: %s\n", out);
-    free(out);
-  });
-  projektionMidiPlayer->getSettings()->defaultBpm = DEFAULT_BPM;
-  projektionMidiPlayer->getSettings()->maxQueueSize = MAX_PLAYREQUESTS;
-  projektionMidiPlayer->getSettings()->maxBuffers = MAX_BUFFERS;
-  projektionMidiPlayer->getSettings()->maxTracks = MAX_TRACKS;
-  projektionMidiPlayer->getSettings()->maxTokensToParse = MAX_TOKENS_PER_TICK;
-  projektionMidiPlayer->getSettings()->defaultMidiChannel = DEFAULT_MIDI_CHANNEL;
-  projektionMidiPlayer->getSettings()->midiStackSize = MIDI_STACK_SIZE;
-  projektionMidiPlayer->getSettings()->midi_gm_bank_msb = MIDI_GM_BANK_MSB;
-  projektionMidiPlayer->getSettings()->midi_gm_bank_lsb = MIDI_GM_BANK_LSB;
+    projektionMidiPlayer = new projektionMidi::projektionMidi();
+    projektionMidiPlayer->setMusicStatusReciever(setMusicStatus);
+    projektionMidiPlayer->setErrorReciever([](const char *msg) {
+        size_t len = strlen(msg);
+        const char *prefix = "(MIDI:ERROR) ";
+        size_t prefixLen = strlen(prefix);
+        char *out = (char *) malloc(len + prefixLen + 1);
+        memcpy(out, prefix, prefixLen);
+        memcpy(out + prefixLen, msg, len + 1);
+        sendIrcMessage(out);
+        Serial.printf("MIDI:ERR: %s\n", out);
+        free(out);
+    });
+    projektionMidiPlayer->getSettings()->defaultBpm = DEFAULT_BPM;
+    projektionMidiPlayer->getSettings()->maxQueueSize = MAX_PLAYREQUESTS;
+    projektionMidiPlayer->getSettings()->maxBuffers = MAX_BUFFERS;
+    projektionMidiPlayer->getSettings()->maxTracks = MAX_TRACKS;
+    projektionMidiPlayer->getSettings()->maxTokensToParse = MAX_TOKENS_PER_TICK;
+    projektionMidiPlayer->getSettings()->defaultMidiChannel = DEFAULT_MIDI_CHANNEL;
+    projektionMidiPlayer->getSettings()->midiStackSize = MIDI_STACK_SIZE;
+    projektionMidiPlayer->getSettings()->midi_gm_bank_msb = MIDI_GM_BANK_MSB;
+    projektionMidiPlayer->getSettings()->midi_gm_bank_lsb = MIDI_GM_BANK_LSB;
 
-  MIDI.begin(4);
-  midiAdapter = new arduinoMidiAdapter(&MIDI, 16);
-  midiAdapter->apply(projektionMidiPlayer, 1);
+    MIDI.begin(4);
+    midiAdapter = new arduinoMidiAdapter(&MIDI, 16);
+    midiAdapter->apply(projektionMidiPlayer, 1);
 
-  psClient.setServer(mqttBrokerI, 1883);
-  psClient.setBufferSize(9216);
-  psClient.setCallback(mqttCallback);
-  psClient.setKeepAlive(120);
-  psClient.setSocketTimeout(120);  
+    psClient.setServer(mqttBrokerI, 1883);
+    psClient.setBufferSize(9216);
+    psClient.setCallback(mqttCallback);
+    psClient.setKeepAlive(120);
+    psClient.setSocketTimeout(120);
 
-  fillPresetSongs(projektionMidiPlayer);
-  fillPresetInstruments(projektionMidiPlayer);
+    fillPresetSongs(projektionMidiPlayer);
+    fillPresetInstruments(projektionMidiPlayer);
 
 #if USE_PROJEKTION_FX
-  projektionMidiPlayer->setFXHandler(&projektionFxHandler);
+    projektionMidiPlayer->setFXHandler(&projektionFxHandler);
 #endif
 
-  setMusicStatus(true);
+    setMusicStatus(true);
 
-  delay(500);
+    delay(500);
 }
 
-void loop()
-{
-  wifictrl.check();
+void loop() {
+    wifictrl.check();
 
-  if (!psClient.connected()) {
-    mqttReconnect();
-  }
-  psClient.loop();
-  ArduinoOTA.handle();
+    if(!psClient.connected()) {
+        mqttReconnect();
+    }
+    psClient.loop();
+    ArduinoOTA.handle();
 
-  projektionMidiPlayer->tick(micros64());
+    projektionMidiPlayer->tick(micros64());
 }
 #endif /* RUN_TEST */
